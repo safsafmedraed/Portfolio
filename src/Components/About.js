795ead6315f94e2b13c54c90e82a66721f6f01dc
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-11-03 23:26:51 +0100

    fix the description and bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,63 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-  render() {
+   bioDisplay = (bio) => {
+      if (bio) {
+         return bio.split('\n').map((item) => <p>{item} <br/></p>);
+      }
+      
+     // 
+   }
+   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = '/CV_SafsafRaed EN_Version.pdf';
-    }
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var profilepic = "images/" + this.props.data.image;
+         var bio = this.props.data.bio;
+         var street = this.props.data.address.street;
+         var city = this.props.data.address.city;
+         var state = this.props.data.address.state;
+         var zip = this.props.data.address.zip;
+         var phone = this.props.data.phone;
+         var email = this.props.data.email;
+         var resumeDownload = '/CV_SafsafRaed EN_Version.pdf';
+      }
 
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="profile-pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
+      return (
+         <section id="about">
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
+               <div className="three columns">
+                  <img className="profile-pic" src={profilepic} alt="profile-pic" />
                </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button" download><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
+               <div className="nine columns main-col">
+                  <h2>About Me</h2>
+
+                  {this.bioDisplay(bio)}
+                 
+                  <div className="row">
+                     <div className="columns contact-details">
+                        <h2>Contact Details</h2>
+                        <p className="address">
+                           <span>{name}</span><br />
+                           <span>{street}<br />
+                              {city} {state}, {zip}
+                           </span><br />
+                           <span>{phone}</span><br />
+                           <span>{email}</span>
+                        </p>
+                     </div>
+                     <div className="columns download">
+                        <p>
+                           <a href={resumeDownload} className="button" download><i className="fa fa-download"></i>Download Resume</a>
+                        </p>
+                     </div>
+                  </div>
                </div>
             </div>
-         </div>
-      </div>
 
-   </section>
-    );
-  }
+         </section>
+      );
+   }
 }
 
 export default About;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,56 +1,61 @@
 import React, { Component } from 'react';
-import ParticlesBg  from "particles-bg";
+import ParticlesBg from "particles-bg";
 
 class Header extends Component {
-  render() {
+   descriptionDisplay = (des) => {
+      if (des) {
+         return des.split('\n').map((item) => <div>{item}<br/></div>)
+      }
+   }
+   render() {
 
-    if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
-      var name = this.props.data.name;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      if (this.props.data) {
+         var project = this.props.data.project;
+         var github = this.props.data.github;
+         var name = this.props.data.name;
+         var description = this.props.data.description;
+         var city = this.props.data.address.city;
+         var networks = this.props.data.social.map(function (network) {
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+         })
+      }
 
-    return (
-      <header id="home">
-      <ParticlesBg type="cobweb" bg={true} color="#ff0000" />
-      <nav id="nav-wrap">
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+      return (
+         <header id="home">
+            <ParticlesBg type="cobweb" bg={true} color="#ff0000" />
+            <nav id="nav-wrap">
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-      </nav>
+               <ul id="nav" className="nav">
+                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                  <li><a className="smoothscroll" href="#about">About</a></li>
+                  <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                  {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
+                  <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               </ul>
+            </nav>
 
-      <div className="row banner">
-      
-         <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
-            <hr />
-            <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-linkedin"></i>Linkedin</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
-            </ul>
-         </div>
-      </div>
+            <div className="row banner">
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+               <div className="banner-text">
+                  <h1 className="responsive-headline">{name}</h1>
+                  <h3>{this.descriptionDisplay(description)}.</h3>
+                  <hr />
+                  <ul className="social">
+                     <a href={project} className="button btn project-btn"><i className="fa fa-linkedin"></i>Linkedin</a>
+                     <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+                  </ul>
+               </div>
+            </div>
 
-   </header>
-    );
-  }
+            <p className="scrolldown">
+               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
+
+         </header>
+      );
+   }
 }
 
 export default Header;
