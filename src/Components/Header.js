8097687db957d0fa1c0b4748b8732ef38fc83d83
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-11-04 15:29:55 +0100

    add background sound and effects

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import ParticlesBg from "particles-bg";
-
+import Sound from "react-sound";
+import Mario from './Extra/MarioBros.mp3'
+import musicButton from './Extra/musicButton.svg'
+import './Extra/customCss.css'
+const headerStyles = {
+   musicbutton: {
+      color: 'black'
+   },
+   musicImg: {
+      paddingLeft: '10px'
+   }
+}
 class Header extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         playing: false,
+         name: 'custombtn',
+      }
+      this.playSound = this.playSound.bind(this)
+   }
+   playSound() {
+
+
+      this.setState({
+         playing: !this.state.playing,
+         name: 'custombtn custombtn--wut is-active'
+      })
+
+
+   }
    descriptionDisplay = (des) => {
       if (des) {
-         return des.split('\n').map((item) => <div>{item}<br/></div>)
+         return des.split('\n').map((item) => <div>{item}<br /></div>)
       }
    }
    render() {
@@ -22,6 +51,10 @@ class Header extends Component {
 
       return (
          <header id="home">
+            <Sound url={Mario}
+               playStatus={this.state.playing ? Sound.status.PLAYING : Sound.status.STOPPED}
+               playFromPosition={1000}
+            />
             <ParticlesBg type="cobweb" bg={true} color="#ff0000" />
             <nav id="nav-wrap">
                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
@@ -36,11 +69,18 @@ class Header extends Component {
                </ul>
             </nav>
 
-            <div className="row banner">
+            <div className="row banner" >
 
                <div className="banner-text">
-                  <h1 className="responsive-headline">{name}</h1>
-                  <h3>{this.descriptionDisplay(description)}.</h3>
+                  <h1 className="responsive-headline" >{name}</h1>
+                  <h3>{this.descriptionDisplay(description)}</h3>
+
+
+
+                  <button className={this.state.playing ? this.state.name : 'custombtn'} onClick={this.playSound} style={headerStyles.musicbutton}>
+                    <b>{this.state.playing ? 'Stop Me !' : 'Play Me ! '} </b> 
+                     <img className={this.state.playing ? 'image' : ''}  src={musicButton} alt='SafsafPortfolio' width='25px' height='25px' style={headerStyles.musicImg} />
+                  </button>
                   <hr />
                   <ul className="social">
                      <a href={project} className="button btn project-btn"><i className="fa fa-linkedin"></i>Linkedin</a>
@@ -52,6 +92,7 @@ class Header extends Component {
             <p className="scrolldown">
                <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
             </p>
+
 
          </header>
       );
