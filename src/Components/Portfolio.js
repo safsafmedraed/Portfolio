1bbb6794f2b9264221498deffdff36fdf89ccbe1
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-11-07 16:35:33 +0100

    add cezrtiification and hover them

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,7 +6,7 @@ class Portfolio extends Component {
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
         var projectImage = 'images/portfolio/' + projects.image;
-        
+
         return (
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
@@ -32,10 +32,13 @@ class Portfolio extends Component {
 
           <div className="twelve columns collapsed">
 
-            {/* <h1>Check Out Some of My Works.</h1> */}
+            <h1>Check Out Some of My Certifications.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {/* projects */}
+            <div className="six columns main-col zoom">
+              <img  src='/images/bootstrapCert.PNG' alt='safsafBootstrap' style={{ borderRadius: '2%' }} />
+            </div>
+            <div className="six columns main-col zoom">
+              <img  src='/images/reactcert.PNG' alt='safsafReact' style={{ borderRadius: '2%' }} />
             </div>
           </div>
         </div>
